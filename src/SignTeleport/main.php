62a4f5d9b70175457f7a42182ca4c7b6cc884a75
narapon <43005911+narapons@users.noreply.github.com>
2021-08-17 19:47:57 +0900

    fix

--- src/SignTeleport/main.php
+++ src/SignTeleport/main.php
@@ -85,7 +85,7 @@ class main extends PluginBase implements Listener
             if($DATAS[5] == NULL) {
                 $msg = "";
             }else{
-                if(preg_match("%name",$DATAS[5]) != false) {
+                if(strstr($DATAS[5],"%name") != false) {
                     $msg = str_replace("%name", $name, $DATAS[5]);
                 }else{
                     $msg = $DATAS[5];
